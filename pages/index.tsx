bbc8fdf64b8b733350d203fdb1c81f0062d0e291
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-01 15:51:44 +0200

    added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,3 @@
-import { Session } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-import Account from "../components/Account";
-import Auth from "../components/Auth";
-import { supabase } from "../utils/supabaseClient";
-
 export default function Home() {
-  const [session, setSession] = useState<Session | null>();
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
-  return (
-    <div className="container p-20">
-      {!session ? (
-        <Auth />
-      ) : (
-        <Account key={session.user?.id} session={session} />
-      )}
-    </div>
-  );
+  return <div className="container p-20">Hello</div>;
 }
