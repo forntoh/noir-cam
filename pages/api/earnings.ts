386598571a8ac6abfd45574498a66162ef5486a3
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-03 00:55:48 +0200

    added stripchat api

---
+++ pages/api/earnings.ts
@@ -0,0 +1,96 @@
+import { endOfWeek, format, parse, startOfWeek } from "date-fns";
+import eachWeekOfInterval from "date-fns/eachWeekOfInterval";
+import nextSunday from "date-fns/nextSunday";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { stripchatApi } from "../../utils/stripchatApi";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { username, start_date },
+    method,
+  } = req;
+
+  try {
+    switch (method) {
+      case "GET":
+        if (start_date) {
+          const result = eachWeekOfInterval(
+            {
+              start: parse(start_date as string, "yyyy-MM-dd", new Date()),
+              end: new Date(),
+            },
+            { weekStartsOn: 1 }
+          );
+
+          const earnings = [];
+
+          for (const date of result) {
+            earnings.push(
+              await fetchStrip(username as string, date, nextSunday(date))
+            );
+          }
+          res.status(200).json(earnings);
+        } else {
+          const today = new Date();
+          const start = startOfWeek(today, { weekStartsOn: 1 });
+          const end = endOfWeek(today, { weekStartsOn: 1 });
+          res
+            .status(200)
+            .json(await fetchStrip(username as string, start, end));
+        }
+        break;
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Internal Server Error");
+  }
+};
+
+const fetchStrip = async (username: string, start: Date, end: Date) => {
+  const { data } = await stripchatApi.get(`/${username}`, {
+    params: {
+      periodStart: format(start, "yyyy-MM-dd"),
+      periodEnd: format(end, "yyyy-MM-dd"),
+    },
+  });
+  sanitize(data);
+  return data;
+};
+
+function sanitize(data: any) {
+  delete data["groupShows"];
+  delete data["refunds"];
+  delete data["modelsReferralProgram"];
+  delete data["otherIncome"];
+  delete data["massMessages"];
+  delete data["photoSalesPM"];
+  delete data["publicShowRecordings"];
+  delete data["spyOnPrivatesVR"];
+  delete data["publicTipsVR"];
+  delete data["usersReferralProgram"];
+  delete data["fanClubPrinces"];
+  delete data["fanClubLords"];
+  delete data["fanClubSoldiers"];
+  delete data["anonymousTipsVR"];
+  delete data["anonymousTips"];
+  delete data["privateChatTipsVR"];
+  delete data["userTipsVR"];
+  delete data["exclusivePrivateNoVideo"];
+  delete data["contestWin"];
+  delete data["spyOnPrivates"];
+  delete data["videoSales"];
+  delete data["albumSales"];
+  delete data["unlockChat"];
+  delete data["offlineTips"];
+  delete data["localRefund"];
+  delete data["tips"];
+  delete data["publicChatTips"];
+  delete data["privateChatTips"];
+  delete data["privateShows"];
+  delete data["exclusivePrivates"];
+  data["periodStart"] = (data["periodStart"] as string).slice(0, 10);
+  data["periodEnd"] = (data["periodEnd"] as string).slice(0, 10);
+}
--- utils/stripchatApi.ts
+++ utils/stripchatApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const stripchatApi = axios.create({
   baseURL:
-    "https://stripchat.com/api/stats/v2/studios/username/strip-ken/models/username/",
+    "https://stripchat.com/api/stats/v2/studios/username/strip-ken/models/username",
   headers: {
     "API-Key": process.env.STRIPCHAT_API_SECRET || "",
     accept: "application/json",
