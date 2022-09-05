d71e0e88e95314e2288136d01d18b1fc87c6178e
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-05 15:33:38 +0200

    added welcome bar

---
+++ components/welcome_bar.tsx
@@ -0,0 +1,15 @@
+function WelcomeBar() {
+  return (
+    <div className="flex justify-between items-center">
+      <div>
+        Hello <b>sweetbooty_one</b>
+      </div>
+      <ul className="flex gap-2 font-semibold">
+        <li>TK</li>
+        <li>USD</li>
+      </ul>
+    </div>
+  );
+}
+
+export default WelcomeBar;
