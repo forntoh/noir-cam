ab772ee36c32d1825874e40afa5e040c82e8026e
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-05 15:52:55 +0200

    added earnings wrapper

---
+++ components/earnings/earning_wrapper.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  label: string;
+  children: React.ReactNode;
+};
+
+function EarningWrapper({ label, children }: Props) {
+  return (
+    <div className="space-y-2">
+      <b className="text-xs">{label}</b>
+      {children}
+    </div>
+  );
+}
+
+export default EarningWrapper;
