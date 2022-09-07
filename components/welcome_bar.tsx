8519177c2430b9fa7067ad05eae639d822a81e65
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-07 20:56:19 +0200

    show username in welcome bar

--- components/welcome_bar.tsx
+++ components/welcome_bar.tsx
@@ -1,16 +1,23 @@
 import { useRecoilState } from "recoil";
+import { useEffectOnce } from "usehooks-ts";
 import { currencyAtom } from "../helpers/helpers";
+import { useModel } from "../hooks/model";
 import { CurrencyType } from "../typings";
 
 const currencies: CurrencyType[] = ["tk", "Ksh"];
 
 function WelcomeBar() {
   const [currency, setCurrency] = useRecoilState(currencyAtom);
+  const [, model, loadModel] = useModel();
+
+  useEffectOnce(() => {
+    loadModel();
+  });
 
   return (
     <div className="flex justify-between items-center">
       <div className="select-none">
-        Hello <b>sweetbooty_one</b>
+        Hello <b>{model?.username ?? "User"}</b>
       </div>
       <ul className="flex gap-2 font-semibold">
         {currencies.map((c, i) => (
