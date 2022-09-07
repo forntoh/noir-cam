fc8ec158a76f806062f5942ee341ee9b46974921
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-07 20:33:20 +0200

    updated fetch model

--- hooks/model/useModel.ts
+++ hooks/model/useModel.ts
@@ -2,9 +2,16 @@ import { Model } from "../../typings";
 import { supabase } from "../../utils/supabaseClient";
 import { runner } from "../../utils/supabaseRunner";
 
-export default (username?: string) =>
-  runner<Model>(() => {
-    return supabase.from("models").select().eq("username", username).single();
+export default () =>
+  runner<Model>((username?: string) => {
+    return supabase
+      .from("models")
+      .select()
+      .eq(
+        username ? "username" : "user_id",
+        username ?? supabase.auth.user()?.id
+      )
+      .single();
   });
 
 export const useModels = () =>
