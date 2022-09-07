ce98bb6f61e481cfb88c6c3339ec55646122a98d
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-07 12:36:01 +0200

    made header sticky

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import MyLink from "./link";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center container py-8">
+    <div className="flex justify-between items-center container py-8 sticky top-0 bg-white bg-opacity-95 z-50">
       <MyLink>
         <h3 className="select-none">
           <span className="font-medium">NOIR</span>CAM
