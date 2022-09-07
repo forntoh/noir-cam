b51d979f60ef0f3d25a8d28a4d2f9f685ffa6d40
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-07 12:36:34 +0200

    added login page

---
+++ pages/login.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Header from "../components/header";
+import { Button, Input } from "../components/input";
+import { supabase } from "../utils/supabaseClient";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (email: string) => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signIn({ email });
+      if (error) throw error;
+      alert("Check your email for the login link!");
+    } catch (error: any) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <Header />
+      <div className="grow"></div>
+      <div className="container flex flex-col justify-end pb-12 gap-8">
+        <p className="leading-6">
+          Sign in via magic link with your email below
+        </p>
+        <Input
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button
+          onClick={async (e) => {
+            e.preventDefault();
+            handleLogin(email);
+          }}
+          className="bg-black text-white"
+          disabled={loading}
+        >
+          <span>{loading ? "Loading" : "Send magic link"}</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
