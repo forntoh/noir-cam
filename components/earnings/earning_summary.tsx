b988ace0659fe60fabb6f4d94a20946b8f502d28
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-10 15:06:52 +0200

    added color to earnings summary

--- components/earnings/earning_summary.tsx
+++ components/earnings/earning_summary.tsx
@@ -7,15 +7,16 @@ type Props = {
   label: string;
   value?: number;
   mCurrency?: CurrencyType;
+  color?: string;
 };
 
-function EarningSummary({ label, value, mCurrency }: Props) {
+function EarningSummary({ label, value, mCurrency, color }: Props) {
   const [currency] = useRecoilState(currencyAtom);
   const amount = mCurrency ? value ?? 0 : converter(value, currency);
 
   return (
     <EarningWrapper label={label}>
-      <h3>
+      <h3 className={`${amount < 0 ? "text-rose-500" : color}`}>
         {amount.toLocaleString() ?? 0}
         <span className="text-sm"> {mCurrency ?? currency}</span>
       </h3>
