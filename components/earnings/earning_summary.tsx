35840de7267ce97c9b8d166315ce059cb16047cb
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-10 14:17:33 +0200

    can fix currency

--- components/earnings/earning_summary.tsx
+++ components/earnings/earning_summary.tsx
@@ -1,21 +1,23 @@
 import { useRecoilState } from "recoil";
 import { converter, currencyAtom } from "../../helpers/helpers";
+import { CurrencyType } from "../../typings";
 import EarningWrapper from "./earning_wrapper";
 
 type Props = {
   label: string;
   value?: number;
+  mCurrency?: CurrencyType;
 };
 
-function EarningSummary({ label, value }: Props) {
+function EarningSummary({ label, value, mCurrency }: Props) {
   const [currency] = useRecoilState(currencyAtom);
-  const amount = converter(value, currency);
+  const amount = mCurrency ? value ?? 0 : converter(value, currency);
 
   return (
     <EarningWrapper label={label}>
       <h3>
         {amount.toLocaleString() ?? 0}
-        <span className="text-sm"> {currency}</span>
+        <span className="text-sm"> {mCurrency ?? currency}</span>
       </h3>
     </EarningWrapper>
   );
