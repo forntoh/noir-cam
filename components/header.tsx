ce0f05328d64897adbcb4e846d063a3ca0df1084
Forntoh Thomas <thomasforntoh@gmail.com>
2022-09-10 15:35:14 +0200

    remove header bg opacity

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { UserMenu } from "./menu";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center container py-8 sticky top-0 bg-white bg-opacity-95 z-50">
+    <div className="flex justify-between items-center container py-8 sticky top-0 bg-white z-50">
       <MyLink>
         <h3 className="select-none">
           <span className="font-medium">NOIR</span>CAM
