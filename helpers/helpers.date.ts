56a30f9101f5a7594a2224568826512bb73f0b2b
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-04 07:16:44 +0100

    updated get montht start and end

--- helpers/helpers.date.ts
+++ helpers/helpers.date.ts
@@ -5,17 +5,22 @@ import {
   format,
   startOfMonth,
   startOfWeek,
+  subDays,
 } from "date-fns";
 
 export const getStartOfMonth = (refDate: Date) => {
   const som = startOfMonth(refDate);
-  return som.getDay() == 1
+  return som.getDay() == 1 // Is Mon
     ? som
+    : som.getDay() == 2 // Is Tue
+    ? subDays(som, 1)
     : addSeconds(getEndOfWeek(startOfMonth(refDate)), 1);
 };
 
-export const getEndOfMonth = (refDate: Date) =>
-  getEndOfWeek(endOfMonth(refDate));
+export const getEndOfMonth = (refDate: Date) => {
+  const mEom = endOfMonth(refDate);
+  return mEom.getDay() == 1 ? subDays(mEom, 1) : getEndOfWeek(mEom);
+};
 
 export const getStartOfWeek = (refDate: Date) =>
   startOfWeek(refDate, { weekStartsOn: 1 });
